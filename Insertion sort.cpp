f42823acec18c87aa444d0b67975ccf21bde3c29
Yashraj Balidani <39310788+rajyash1904@users.noreply.github.com>
2020-03-31 15:06:22 +0530

    Add files via upload

---
+++ Insertion sort.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void
+printarr (vector < int >&a)
+{
+
+  for (int i = 0; i < a.size (); i++)
+    {
+      cout << a[i] << " ";
+    }
+  cout << endl;
+}
+
+
+void
+swap (vector < int >&a, int j, int i)
+{
+  int t = a[j];
+  a[j] = a[i];
+  a[i] = t;
+  return;
+}
+
+void insertion_sort(vector<int> &a){
+    int n=a.size();
+    
+    for(int i=1;i<n;i++){
+        int key=a[i];
+        int j=i-1;
+        while(j>=0 && a[j]>key){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=key;
+    }
+    printarr(a);
+    return;
+    
+}
+
+int
+main ()
+{
+
+  vector < int >a = { 25, 360, 39, 9000, 1000 };
+  selection_sort (a);
+  return 0;
+}
